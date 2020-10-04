a6a05bf071e9767e8d2f83b6071703c57cffa08d
mickuz <m.kuxniewicz@gmail.com>
2020-10-04 23:13:28 +0200

    Obtain the authorization for application
    Generated a token to get user authorization enabling requests to the Spotify platform.

---
+++ config.py
@@ -0,0 +1,8 @@
+SPOTIPY_CLIENT_ID = 'your-client-id'
+SPOTIPY_CLIENT_SECRET = 'your-client-secret'
+
+username = 'your-username'
+
+scope = 'playlist-read-private'
+
+redirect_uri = 'http://localhost:8887/callback/'

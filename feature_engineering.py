4bac2ca6df23dccd8ed1117b4b8bf8f08b415513
mickuz <m.kuxniewicz@gmail.com>
2020-12-17 19:50:27 +0100

    Fix a function applying one hot encoding
    Now the function correctly adds new features to the DataFrame.

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,10 +1,15 @@
-from sklearn.preprocessing import StandardScaler, OneHotEncoder, KBinsDiscretizer
+import re
+import pandas as pd
+
+from sklearn.preprocessing import (StandardScaler, OneHotEncoder,
+                                   KBinsDiscretizer)
 
 
 def standardize_features(df, cols=[], if_train=True):
     df_copy = df.copy()
 
     scaler = StandardScaler()
+
     if if_train:
         df_copy[cols] = scaler.fit_transform(df[cols])
     else:
@@ -19,20 +24,45 @@ def discretize_features(df, cols=[], if_train=True, cat_number=5):
     discretizer = KBinsDiscretizer(strategy='uniform',
                                    n_bins=cat_number,
                                    encode='ordinal')
+
     if if_train:
-        df_copy[cols] = discretizer.fit_transform(df[cols])
+        df_copy[cols] = discretizer.fit_transform(df[cols]).astype(int)
     else:
-        df_copy[cols] = discretizer.transform(df[cols])
+        df_copy[cols] = discretizer.transform(df[cols]).astype(int)
 
     return df_copy
 
 
 def encode_features(df, cols=[], if_train=True):
-    df_copy = df.copy()
+    encoder = OneHotEncoder(handle_unknown='ignore',
+                            sparse=False)
 
-    encoder = OneHotEncoder(drop='first',
-                            handle_unknown='ignore')
+    def add_dummy_vars(encoder, transformed_features):
+        df_copy = df.copy()
+
+        df_transformed = pd.DataFrame(transformed_features,
+                                      columns=encoder.get_feature_names(cols),
+                                      index=df_copy.index)
+
+        df_copy = pd.concat([df_copy, df_transformed], axis=1)
+
+        df_copy.drop(cols, axis=1, inplace=True)
+        for col in cols:
+            regex = '{}.*_\d*$'.format(col)
+            pattern = re.compile(regex)
+
+            selected_cols = [col for col in list(df_copy.columns)
+                             if pattern.match(col)]
+
+            df_copy.drop(selected_cols[0], axis=1, inplace=True)
+
+        return df_copy
+
     if if_train:
-        df_copy[cols] = encoder.fit_transform(df[cols])
+        transformed = encoder.fit_transform(df[cols]).astype(int)
+        df = add_dummy_vars(encoder, transformed)
     else:
-        df_copy[cols] = encoder.transform(df[cols])
+        transformed = encoder.transform(df[cols]).astype(int)
+        df = add_dummy_vars(encoder, transformed)
+
+    return df

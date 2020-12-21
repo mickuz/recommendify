e17b1e6dd674804dc7596c418c7b9aab2cf10540
mickuz <m.kuxniewicz@gmail.com>
2020-12-21 20:34:57 +0100

    Move main.py into raw_dataset.py and change format of configuration file
    Added content of main.py file in location such that it suits the structure of the project. Utilized YAML configuration file to facilitate changes of parameters in the project.

--- src/configs/config.py
+++
@@ -1,8 +0,0 @@
-SPOTIPY_CLIENT_ID = 'your-client-id'
-SPOTIPY_CLIENT_SECRET = 'your-client-secret'
-
-username = 'your-username'
-
-scope = 'playlist-read-private'
-
-redirect_uri = 'http://localhost:8887/callback/'
---
+++ src/data/raw_dataset.py
@@ -0,0 +1,31 @@
+import os
+import yaml
+
+from data_fetcher import SpotifyDataFetcher
+from dataframes_merger import merge_playlist_dataframes
+
+
+def main():
+    with open('../configs/config.yaml', mode='r') as config_file:
+        conf = yaml.load(config_file, Loader=yaml.FullLoader)
+
+    id = os.environ['CLIENT_ID']
+    secret = os.environ['CLIENT_SECRET']
+
+    username = conf['api']['username']
+    scope = conf['api']['scope']
+    uri = conf['api']['uri']
+
+    like = conf['playlists']['like']
+    dislike = conf['playlists']['dislike']
+
+    raw_path = conf['data']['raw-dataset']
+
+    sdf = SpotifyDataFetcher(id, secret, username, scope, uri)
+    df = merge_playlist_dataframes(sdf, username, like, dislike)
+
+    df.to_csv(raw_path, index=True)
+
+
+if __name__ == '__main__':
+    main()

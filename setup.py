a089f317a0e6edd05c05a7b260d398aee912e425
Michał Kuźniewicz <m.kuxniewicz@gmail.com>
2021-01-12 21:53:47 +0100

    Package the project
    Added files necessary to build a package with the project.

---
+++ setup.py
@@ -0,0 +1,40 @@
+from distutils.core import setup
+
+setup(name='Recommendify',
+      version='0.1',
+      description='Spotify Recommender System',
+      author='Michał Kuźniewicz',
+      author_email='michal.kuzniewicz@tuta.io',
+      url='https://github.com/mickuz/recommendify',
+      packages=['src'],
+      install_requires=[
+            'spotipy',
+            'PyYAML',
+            'pandas',
+            'numpy',
+            'scikit-learn'
+      ],
+      extras_require={
+            'plotting': ['matplotlib', 'seaborn', 'jupyter'],
+            'optimization': ['xgboost', 'hyperopt']
+      },
+      setup_requires=[
+            'flake8',
+            'pytest-runner'
+      ],
+      tests_require=[
+            'pytest',
+            'pytest-cov',
+            'pytest-mock'
+      ],
+      entry_points={
+            'console_scripts': [
+                  'fetch-data=src.data.raw_dataset:main',
+                  'process-data=src.features.processed_dataset:main',
+                  'train=src.models.train_model:main',
+                  'predict=src.main:main'
+            ]
+      },
+      package_data={
+            'src': ['configs/config.yaml']
+      })
